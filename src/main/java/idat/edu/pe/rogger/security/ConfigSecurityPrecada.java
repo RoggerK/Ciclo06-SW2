3e48512bd817437709472aa17dda4e83f8473ad1
RoggerK <at76324897@idat.edu.pe>
2022-11-09 10:39:20 -0500

    config security-basic

--- src/main/java/idat/edu/pe/rogger/security/ConfigSecurityPrecada.java
+++ src/main/java/idat/edu/pe/rogger/security/ConfigSecurityPrecada.java
@@ -1,76 +1,38 @@
 package idat.edu.pe.rogger.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ConfigSecurityPrecada extends WebSecurityConfigurerAdapter {
-	@Autowired
-	private UserDetailService service;
-	
-	@Autowired
-	private TokenFilter filter;
-	
-	@Autowired
-	private EntryPoint entrypoint;
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		// TODO Auto-generated method stub
-		return super.authenticationManagerBean();
-	}
-
-	@Override
-	protected AuthenticationManager authenticationManager() throws Exception {
-		// TODO Auto-generated method stub
-		return super.authenticationManager();
-	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//		auth.inMemoryAuthentication().withUser("PROFESOR").password(encriptado().encode("123")).roles("ADMIN");
-//		auth.inMemoryAuthentication().withUser("ALUMNO").password(encriptado().encode("123")).roles("USER");
-		auth.userDetailsService(service).passwordEncoder(encriptado());
+		auth.inMemoryAuthentication().withUser("PROFESOR").password(encriptado().encode("123")).roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("ALUMNO").password(encriptado().encode("123")).roles("USER");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-//		http.authorizeRequests()
-//		.antMatchers("/producto/v1/listar").hasAnyRole("ADMIN", "USER")
-//		.antMatchers("/producto/v1/listar/**").hasAnyRole("ADMIN", "USER")
-//		.antMatchers("/producto/v1/guardar").hasAnyRole("ADMIN")//.access("hasRole('ADMIN')")
-//		.antMatchers("/producto/v1/editar").hasAnyRole("ADMIN")
-//		.antMatchers("/producto/v1/eliminar/**").hasAnyRole("ADMIN")
-//		.and()
-//		.httpBasic()
-//		.and()
-//		.csrf().disable();
-		
-			http.authorizeHttpRequests()
-			.antMatchers("/crearToken").permitAll()
-			.anyRequest()
-			.authenticated()
-			.and()
-			.exceptionHandling()
-			.authenticationEntryPoint(entrypoint)
-			.and()
-			.sessionManagement()
-			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and()
-			.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
-			.csrf().disable();
+		http.authorizeRequests()
+		.antMatchers("/producto/v1/listar").hasAnyRole("ADMIN", "USER")
+		.antMatchers("/producto/v1/listar/**").hasAnyRole("ADMIN", "USER")
+		.antMatchers("/producto/v1/guardar").hasAnyRole("ADMIN") //.access("hasRole('ADMIN')")
+		.antMatchers("/producto/v1/editar").hasAnyRole("ADMIN")
+		.antMatchers("/producto/v1/eliminar/**").hasAnyRole("ADMIN")
+		.and()
+		.httpBasic()
+		.and()
+		.csrf().disable();
 	}
 	
 	@Bean
